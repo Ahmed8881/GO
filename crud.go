422b1fdd4e4c4d5d28f02e11b9980041560e9fc5
Ahmed8881 <buttahmed2004@gmail.com>
2024-12-27 19:03:32 +0500

    add nextID variable to track user IDs

--- crud.go
+++ crud.go
@@ -10,3 +10,14 @@ type User struct {
 	Age int
 }
 var users []User
+var nextID = 1
+func createUser(name string, age int) User {
+    user := User{
+        ID:   nextID,
+        Name: name,
+        Age:  age,
+    }
+    users = append(users, user)
+    nextID++
+    return user
+}
